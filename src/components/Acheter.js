7362fdb863d73a3d6790b59324629fc904886c20
unknown <essidmohamedamin@gmail.com>
2021-04-24 23:53:43 +0200

    change api call

--- src/components/Acheter.js
+++ src/components/Acheter.js
@@ -1,19 +1,8 @@
 import React, { useState, useRef } from "react";
-import { Formik, Form, Field } from "formik";
 import axios from "axios";
 import { Row, Col, Button } from "react-bootstrap";
 import Remove from "../images/remove.png";
-import * as Yup from "yup";
 // import { useForm } from "@formspree/react";
-
-const formSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email").required("Required"),
-  message: Yup.string().required("Required"),
-  produit: Yup.string().required("Required"),
-  nom: Yup.string().required("Required"),
-  phone: Yup.string().required("Required"),
-  adresse: Yup.string().required("Required"),
-});
 
 const Acheter = ({
   produit,
@@ -21,7 +10,6 @@ const Acheter = ({
   setPopupClass,
   setSuccessPopupClass,
 }) => {
-  const formEl = useRef(null);
   const [serverState, setServerState] = useState({
     submitting: false,
     status: null,
@@ -72,101 +60,6 @@ const Acheter = ({
             </Col>
           </Row>
           <br />
-          {/* <Formik
-            initialValues={{
-              produit,
-              nom: "",
-              phone: "",
-              adresse: "",
-              email: "",
-            }}
-            onSubmit={(values, actions) => {
-              console.log(process.env.GATSBY_FORMSPREE);
-              let data = JSON.stringify({
-                produit: values.produit,
-                nom: values.nom,
-                phone: values.phone,
-                adresse: values.adresse,
-                email: values.email,
-              });
-              let d = actions.Form;
-              console.log(d);
-              console.log(formEl.current);
-              let axiosConfig = {
-                headers: {
-                  "Content-Type": "application/json;charset=UTF-8",
-                  "Access-Control-Allow-Origin": "*",
-                },
-              };
-              axios({
-                method: "POST",
-                url: process.env.GATSBY_FORMSPREE,
-                data: values,
-                axiosConfig,
-              })
-                .then(function (response) {
-                  actions.setSubmitting(false);
-                  console.log(response);
-                  console.log(values);
-                  setPopupClass("overlay_hidden");
-                  setSuccessPopupClass("overlay");
-                })
-                .catch(function (error) {
-                  actions.setSubmitting(false);
-                  console.log(error);
-                });
-              console.log({
-                produit: values.produit,
-                nom: values.nom,
-                phone: values.phone,
-                adresse: values.adresse,
-                email: values.email,
-              });
-              setPopupClass("overlay_hidden");
-              setSuccessPopupClass("overlay");
-
-              // var fd = new FormData({
-              //   produit: values.produit,
-              //   nom: values.nom,
-              //   phone: values.phone,
-              //   adresse: values.adresse,
-              //   email: values.email,
-              // });
-              // handleSubmit(fd);
-            }}
-            // validationSchema={formSchema}
-          >
-            {() => (
-              <Form ref={formEl}>
-                <div className="form-group">
-                  <label htmlFor="nom">Nom: </label>
-                  <Field className="form-control" name="nom" />
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="phone">Numero du téléphone: </label>
-                  <Field className="form-control" name="phone" />
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="adresse">Adresse: </label>
-                  <Field className="form-control" name="adresse" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email">Email: </label>
-                  <Field className="form-control" name="email" />
-                </div>
-
-                <Button
-                  style={{ backgroundColor: "red", fontSize: "24px" }}
-                  variant="primary"
-                  type="submit"
-                >
-                  Acheter
-                </Button>
-              </Form>
-            )}
-          </Formik> */}
 
           <form onSubmit={handleOnSubmit}>
             <div style={{ display: "none" }} className="form-group">
@@ -190,7 +83,7 @@ const Acheter = ({
             </div>
             <div className="form-group">
               <label htmlFor="email">Email: </label>
-              <input className="form-control" name="email" />
+              <input type="text" className="form-control" name="email" />
             </div>
 
             <Button
